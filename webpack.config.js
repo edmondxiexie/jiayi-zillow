496c0a6d66e05811281b6964e5b2e5f5eab39a23
edmondxiexie <xiejy36@gmail.com>
2018-05-22 05:37:48 -0700

    Add .js and .jsx loader. Add a unique hash to output bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 var webpack = require("webpack");
 var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: "bundle.js"
-  }
+    filename: "bundle.[chunkhash].js"
+  },
+  module: {
+    rules: [
+      {
+        use: "babel-loader",
+        test: /\.jsx?$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html"
+    })
+  ]
 };

38dc014ebeffbe0527a24afcb0d87de09793ad6e
edmondxiexie <xiejy36@gmail.com>
2018-05-22 16:33:03 -0700

    Add webpack-dev-server for development

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ var webpack = require("webpack");
 var path = require("path");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
   entry: "./src/index.js",
   output: {
